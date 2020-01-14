43b10297de5d7112899caaaeaae9e05ce2f9afd4
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-14 14:56:53 +0100

    Minor fixes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 // bot generates 16 random numbers between 1 and 100
-var botNumbers = [];
-for (var i = 0; i < 16; i++) {
-  botNumbers.push(parseInt(getRndInteger(1, 100)));
-}
-botNumbers.sort();
+var botNumbers = arrayGenerator(16, 1, 1000);
 console.log(botNumbers);
+
+// how do i prevent the array to have cloned numbers??
 
 // ask user to insert for 84 times a number between 1 and 100
 
@@ -17,4 +15,14 @@ console.log(botNumbers);
 // random number generator between min and max
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// random array generator with selected amount of numbers between min and max
+function arrayGenerator(arrayLength, min, max) {
+  var arrayNumbers = [];
+  for (var i = 0; i < arrayLength; i++) {
+    arrayNumbers.push(parseInt(getRndInteger(min, max)));
+  }
+  arrayNumbers.sort(function(a, b){return a-b});
+  return arrayNumbers;
 }
