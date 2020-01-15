e1316839b06cab6c6dec97adb808bda17af038c5
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-15 17:28:53 +0100

    Finished main exercise

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var i = 0;
 var level = '';
 var minNumber = 1;
 var maxNumber = 100;
-var attempt = 5;
+var attempt = 84;
 var arrayBomb = [];
 var numberBomb = 0;
 var arrayUser = [];
@@ -14,6 +14,8 @@ var score = 0;
 // level 0 > 1 to 100 with 84 times
 // level 1 > 1 to 80 with 64 times
 // level 2 > 1 to 50 with 34 times
+// level 9 > 1 to 20 with 5 times, only for debug
+
 do {
   level = parseInt(prompt('Select a level difficulty: 0 or 1 or 2'));
   if (level === 0) {
@@ -24,8 +26,12 @@ do {
   } else if (level === 2) {
     maxNumber = 50;
     attempt = 34;
+  } else if (level === 9) {
+    maxNumber = 50;
+    attempt = 5;
   }
-} while (level !== 0 && level !== 1 && level !== 2);
+
+} while (level !== 0 && level !== 1 && level !== 2 && level !== 9);
 console.log('level set to', level);
 console.log('minNumber set to', minNumber);
 console.log('maxNumber set to', maxNumber);
@@ -39,18 +45,17 @@ for (i = 0; arrayBomb.length < 16; i++) {
   }
 }
 arrayBomb.sort(function(a, b){return a-b});
+console.log('The bombs are here');
 console.log(arrayBomb);
 
 // ask user n attempt times to insert a number between  and maxNumber
 i = 0;
 while (i < attempt && arrayScan(arrayBomb, numberUser) === false) {
-
   // ask numberUser and push it inside arrayUser
   // check isBetween minNumber and maxNumber and that isn't inside arrayUser
   do {
     numberUser = parseInt(prompt('Insert a number between ' + minNumber + ' and ' + maxNumber));
   } while (isBetween(minNumber, maxNumber, numberUser) === false || arrayScan(arrayUser, numberUser));
-
   // only push inside the array if is not a numberBomb
   if (arrayScan(arrayBomb, numberUser) === false) {
     arrayUser.push(numberUser);
@@ -58,14 +63,11 @@ while (i < attempt && arrayScan(arrayBomb, numberUser) === false) {
   }
   i++;
 }
+console.log('These are the user choices');
 console.log(arrayUser);
-console.log(score);
 
-// if number is on the main list, gave over
-
-// otherwise keep asking numbers 84 times
-
-// keep score of everything
+// display final score
+console.log('Your final score is', score);
 
 
 // function
